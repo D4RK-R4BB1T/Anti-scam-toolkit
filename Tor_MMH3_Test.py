d16feaefb542eef8e0da914be858f0ed2a8bf9b4
Pavel M. Kravkenko <91730678+D4RK-R4BB1T@users.noreply.github.com>
2023-05-07 20:56:35 -0400

    Update Tor_MMH3_Test.py

--- Tor_MMH3_Test.py
+++ Tor_MMH3_Test.py
@@ -1,78 +1,67 @@
-
 import hashlib
-import requests
 import os
-import subprocess
-import sys
+import requests
 import stem.process
-
+import sys
 
 def install_libraries():
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "hashlib", "stem"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "hashlib", "requests", "stem"])
         print("Libraries installed successfully.")
 
     except subprocess.CalledProcessError:
         print("Failed to install libraries. Please make sure you have pip installed.")
         sys.exit(1)
 
-
-def calculate_file_hash(file_path):
+def install_tor():
     try:
-        with open(file_path, 'rb') as file:
-            file_content = file.read()
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "stem"])
+        print("Tor installed successfully.")
 
-        mmh3_hash = hashlib.mmh3(file_content)
-        sha1_hash = hashlib.sha1(file_content).hexdigest()
-        sha256_hash = hashlib.sha256(file_content).hexdigest()
-        sha512_hash = hashlib.sha512(file_content).hexdigest()
-
-        print("MMH3 Hash:", mmh3_hash)
-        print("SHA1 Hash:", sha1_hash)
-        print("SHA256 Hash:", sha256_hash)
-        print("SHA512 Hash:", sha512_hash)
-
-    except FileNotFoundError:
-        print("File not found. Please provide a valid file path.")
-
-
-def download_file(url, file_extension):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            file_content = response.content
-
-            file_name = "downloaded_file" + file_extension
-            with open(file_name, 'wb') as file:
-                file.write(file_content)
-
-            print("File downloaded successfully as", file_name)
-            calculate_file_hash(file_name)
-
-        else:
-            print("Failed to download file. Invalid URL or resource not found.")
-
-    except requests.exceptions.RequestException:
-        print("Failed to download file. Connection error.")
-
+    except subprocess.CalledProcessError:
+        print("Failed to install Tor. Please make sure you have pip installed.")
+        sys.exit(1)
 
 def configure_tor():
     try:
-        tor_process = stem.process.launch_tor_with_config(
-            config={
-                'SocksPort': '9050',
-                'ControlPort': '9051',
-                'Log': 'notice stdout',
-            },
-            tor_cmd='tor'
-        )
-        print("Tor configured successfully.")
+        tor_process = stem.process.launch_tor()
+        print("Tor started successfully.")
         return tor_process
 
     except stem.SocketError:
-        print("Tor not found. Please install Tor from https://www.torproject.org/")
-        sys.exit(1)
+        print("Tor not found. Installing Tor...")
+        install_tor()
+        print("Tor installed successfully.")
+        return configure_tor()
 
+def download_file(url, filename):
+    try:
+        response = requests.get(url, stream=True)
+
+        with open(filename, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file.write(chunk)
+        print("File downloaded successfully.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: Failed to download file - {str(e)}")
+
+def download_hidden_service_html(url):
+    tor_process = configure_tor()
+    with tor_process:
+        try:
+            hidden_service_url = url + ".onion"
+            response = requests.get(hidden_service_url)
+            if response.status_code == 200:
+                filename = url.split("/")[-1] + ".html"
+                with open(filename, "w") as file:
+                    file.write(response.text)
+                print("HTML file downloaded successfully.")
+            else:
+                print("Error: Failed to access hidden service HTML.")
+        except requests.exceptions.RequestException as e:
+            print(f"Error: Failed to access hidden service - {str(e)}")
 
 # Check if the required libraries are installed
 try:
@@ -94,28 +83,11 @@ except ImportError:
         else:
             print("Invalid choice. Please enter 'y' or 'n'.")
 
+# Option 1: Download HTML file from Tor hidden service
+url = input("Enter the URL of the Tor hidden service (without .onion): ")
+download_hidden_service_html(url)
 
-# Configure Tor
-tor_process = configure_tor()
-
-# Main menu
-while True:
-    print("---- Main Menu ----")
-    print("1. Download and calculate hashes of a file")
-    print("2. Quit")
-
-    choice = input("Select an option (1 or 2): ")
-
-    if choice == "1":
-        file_url = input("Enter the URL of the file to download: ")
-        download_file(file_url, os.path.splitext(file_url)[1])
-
-    elif choice == "2":
-        break
-
-    else:
-        print("Invalid choice. Please select a valid option.")
-
-# Stop the Tor process
-tor_process.kill()
-
+# Option 2: Download favicon.ico from Tor hidden service
+url = input("Enter the URL of the Tor hidden service favicon.ico: ")
+filename = input("Enter the filename for the downloaded favicon.ico: ")
+download_file(url, filename)
