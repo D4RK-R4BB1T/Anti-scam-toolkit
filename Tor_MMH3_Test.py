4afb35adc3dd13d5fd2e445bd149eff671f57c61
Pavel M. Kravkenko <91730678+D4RK-R4BB1T@users.noreply.github.com>
2023-05-07 21:09:49 -0400

    Update Tor_MMH3_Test.py

--- Tor_MMH3_Test.py
+++ Tor_MMH3_Test.py
@@ -2,7 +2,9 @@ import hashlib
 import os
 import requests
 import stem.process
+import stem.util.log
 import sys
+import subprocess
 
 def install_libraries():
     try:
@@ -34,21 +36,49 @@ def configure_tor():
         print("Tor installed successfully.")
         return configure_tor()
 
-def download_file(url, filename):
+def is_tor_running():
     try:
-        response = requests.get(url, stream=True)
+        socks_port = stem.util.log.get_localhost().find_free_port()
+        control_port = stem.util.log.get_localhost().find_free_port()
+        tor_process = stem.process.launch_tor_with_config(
+            config={
+                'SocksPort': str(socks_port),
+                'ControlPort': str(control_port),
+            },
+            take_ownership=True
+        )
+        tor_process.kill()
+        return True
 
-        with open(filename, "wb") as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    file.write(chunk)
-        print("File downloaded successfully.")
+    except stem.SocketError:
+        return False
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: Failed to download file - {str(e)}")
+def start_tor():
+    try:
+        tor_process = stem.process.launch_tor()
+        print("Tor started successfully.")
+        return tor_process
+
+    except stem.SocketError:
+        print("Failed to start Tor. Please make sure Tor is installed properly.")
+        sys.exit(1)
+
+def calculate_hash(data, algorithm):
+    hash_obj = hashlib.new(algorithm)
+    hash_obj.update(data)
+    return hash_obj.hexdigest()
 
 def download_hidden_service_html(url):
-    tor_process = configure_tor()
+    if not url.endswith(".onion"):
+        print("Error: Invalid hidden service URL.")
+        return
+
+    if not is_tor_running():
+        print("Tor is not running. Starting Tor...")
+        tor_process = start_tor()
+    else:
+        print("Tor is already running.")
+
     with tor_process:
         try:
             hidden_service_url = url + ".onion"
@@ -58,6 +88,12 @@ def download_hidden_service_html(url):
                 with open(filename, "w") as file:
                     file.write(response.text)
                 print("HTML file downloaded successfully.")
+
+                # Generate Shodan link
+                html_hash = calculate_hash(response.content, "md5")
+                shodan_link = f"https://www.shodan.io/search?query=http.html_hash%3A{html_hash}"
+                print("Shodan Link:", shodan_link)
+
             else:
                 print("Error: Failed to access hidden service HTML.")
         except requests.exceptions.RequestException as e:
@@ -77,11 +113,19 @@ except ImportError:
         if choice.lower() == "y":
             install_libraries()
             break
-        elif choice.lower() == "n":
+       
+elif choice.lower() == "n":
             print("Cannot proceed without the required libraries. Exiting.")
             sys.exit(1)
         else:
             print("Invalid choice. Please enter 'y' or 'n'.")
+
+# Check if Tor is running or start it if necessary
+if not is_tor_running():
+    print("Tor is not running. Starting Tor...")
+    tor_process = start_tor()
+else:
+    print("Tor is already running.")
 
 # Option 1: Download HTML file from Tor hidden service
 url = input("Enter the URL of the Tor hidden service (without .onion): ")
