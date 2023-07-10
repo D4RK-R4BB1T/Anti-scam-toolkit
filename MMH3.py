d5261ac9f1ecbfc0b49bbf32a7cd3c543e4a8b60
Pavel M. Kravkenko <91730678+D4RK-R4BB1T@users.noreply.github.com>
2023-07-09 20:45:09 -0400

    Update MMH3.py

--- MMH3.py
+++ MMH3.py
@@ -1,9 +1,7 @@
 import mmh3
 import hashlib
 import requests
-import os
 import subprocess
-import sys
 
 
 def calculate_file_hash(file_path):
@@ -25,6 +23,29 @@ def calculate_file_hash(file_path):
         print("File not found. Please provide a valid file path.")
 
 
+def is_tor_running():
+    try:
+        result = subprocess.run(["tor", "--version"], capture_output=True, text=True)
+        return result.returncode == 0
+    except FileNotFoundError:
+        return False
+
+
+# Function to make requests through Tor proxy
+def make_tor_request(url):
+    # Use Tor SOCKS proxy
+    proxies = {
+        "http": "socks5h://localhost:9050",
+        "https": "socks5h://localhost:9050"
+    }
+    try:
+        response = requests.get(url, proxies=proxies)
+        return response
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", str(e))
+        return None
+
+
 # Main menu
 while True:
     print("---- Main Menu ----")
@@ -40,27 +61,36 @@ while True:
         calculate_file_hash(file_path)
 
     elif choice == "2":
-        url = input("Enter the URL of the website (supporting HTTP and HTTPS): ")
+        url = input("Enter the URL of the website (supporting HTTP, HTTPS, or .onion): ")
         file_extension = input("Enter the file extension (.ico or .html): ")
 
         if file_extension.lower() not in [".ico", ".html"]:
             print("Invalid file extension. Only .ico and .html files are supported.")
         else:
-            # Download file and calculate hash
-            response = requests.get(url)
-            if response.status_code == 200:
-                file_content = response.content
+            if is_tor_running():
+                if url.endswith(".onion"):
+                    # Make request through Tor proxy
+                    response = make_tor_request(url)
+                else:
+                    # Make regular HTTP/HTTPS request
+                    response = requests.get(url)
 
-                # Save file locally
-                file_name = "downloaded_file" + file_extension
-                with open(file_name, 'wb') as file:
-                    file.write(file_content)
+                if response is not None and response.status_code == 200:
+                    file_content = response.content
 
-                print("File downloaded successfully as", file_name)
-                calculate_file_hash(file_name)
+                    # Save file locally
+                    file_name = "downloaded_file" + file_extension
+                    with open(file_name, 'wb') as file:
+                        file.write(file_content)
+
+                    print("File downloaded successfully as", file_name)
+                    calculate_file_hash(file_name)
+
+                else:
+                    print("Failed to download file. Invalid URL or resource not found.")
 
             else:
-                print("Failed to download file. Invalid URL or resource not found.")
+                print("Tor is not running. Please start Tor and try again.")
 
     elif choice == "3":
         repo_url = input("Enter the GitHub repository URL: ")
